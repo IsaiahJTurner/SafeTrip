d001cc9f7ba87caffbfa18db284b23fb751e18f6
Patrick Murray <patrick@patmurraydev.com>
2016-10-09 05:07:34 -0400

    Merging UI and data

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,157 @@
-// Your code goes here
+var latAcclElem = document.getElementById('turning');
+var forwardAcclElem = document.getElementById('acceleration');
 
-var vinElem = document.getElementById('vin');
+var drivingScoreNotif = document.getElementById('driving-score-lower');
 var drivingScoreElem = document.getElementById('driving-score');
-var accelerationElem = document.getElementById('acceleration');
-var turningElem = document.getElementById('turning');
 
-drivingScoreElem.innerHTML = '90';
-accelerationElem.innerHTML = '80%';
-turningElem.innerHTML = '100%';
 
-gm.info.getVehicleConfiguration(function(data) {
-  vinElem.innerHTML = gm.info.getVIN();
-});
+var speedVar = 0;
+var wheelVar = 0.00;
+var secondsTimestamp = 0;
+var lateralAcceleration = 0;
+var yaw = 0;
+var points = 100;
+
+var alerts = [];
+
+console.log('test date:', Date.now());
+
+addDrivingAlert("Rough braking incident--")
+
+
+// Call processData will all available signals. Expect a 5+ second delay before callback is triggered
+//gm.info.getVehicleData(processData);
+
+// Call processData with only 'engine_oil_temp' signal. Callback triggered much faster with fewer signals
+ gm.info.getVehicleData(processData, ['gps_speed', 'wheel_angle']);
+ gm.info.watchVehicleData(processData, ['gps_speed', 'wheel_angle']);
+
+  // setInterval(cleanupOldAlerts, 10 * 1000);
+  // setTimeout(fuckShitUp, 7 * 1000);
+  // setTimeout(fuckShitUpAgain, 13 * 1000);
+
+  function fuckShitUp() {
+    addDrivingAlert("Took turn too fast--")
+  }
+
+  function fuckShitUpAgain() {
+    addDrivingAlert("You crashed--")
+  }
+
+function processData(data) {
+    console.log('got vehicle data: ', data);
+
+    if (data.wheel_angle != null) {
+        wheelVar = data.wheel_angle;
+    }
+
+    if (data.gps_speed != null) {
+
+        var deltaT = 0;
+        var now = Math.floor(Date.now() / 1000);
+        if (now > secondsTimestamp) {
+            deltaT = now - secondsTimestamp;
+        } else if (now < secondsTimestamp) {
+            deltaT = secondsTimestamp - now;
+        } else {
+            deltaT = 0.00;
+        }
+
+        console.log('seconds: ', now, ' -- deltaT: ', deltaT);
+
+        calculateAcceleration(speedVar, data.gps_speed, deltaT);
+        speedVar = data.gps_speed;
+        deltaT = now;
+    }
+
+    calculateLateralAcceleration(speedVar);
+    //  calcGForce(speedVar, wheelVar);
+
+}
+
+function calculateAcceleration(vi, vf, timeInterval) {
+    var a = ((vi * 3.6) - (vf * 3.6)) / timeInterval;
+    console.log(a);
+    forwardAcclElem.innerHTML = a;
+}
+
+function calculateLateralAcceleration(v) {
+    console.log('wheelVar:', wheelVar);
+    var r = (2048 * 10) / wheelVar;
+    console.log('r:', r);
+    console.log('v in km/h:', v);
+    console.log('v in m/s:', v / 3.6);
+    console.log('v squared:', Math.pow((v / 3.6), 2.0));
+    lateralAcceleration = Math.pow((v / 3.6), 2.0) / Math.abs(r);
+    latAcclElem.innerHTML = Math.floor(lateralAcceleration, 3);
+}
+
+function cleanupOldAlerts() {
+  console.log('we trynna cleanup...', alerts.length);
+
+  var indicesToSplice = [];
+
+  for (var i = 0; i < alerts.length; i++) {
+    console.log('we at num:', i)
+    if ((Date.now() - alerts[i].secondsTimestamp) > 10 * 1000) {
+      console.log('found sumthing old');
+      //been time interval since alert, return points
+      indicesToSplice.push(i);
+
+      console.log('stuff', points);
+
+    }
+  }
+
+  for (var x = 0; x < indicesToSplice.length; x++) {
+    console.log('indicesToSplice:', indicesToSplice[x]);
+    alerts.splice(indicesToSplice[x]);
+  }
+
+  updateDrivingRecordBanner();
+
+  if (points < 100) {
+    console.log('adding points from', points);
+    points = points + (10 * indicesToSplice.length);
+    drivingScoreElem.innerHTML = points;
+  }
+}
+
+function updateDrivingRecordBanner() {
+  var hi = "";
+  for (var i = 0; i < alerts.length; i++) {
+    hi = hi.concat(alerts[i].alertType);
+  }
+  drivingScoreNotif.innerHTML = hi;
+}
+
+function addDrivingAlert(type) {
+
+  var alert = {
+    alertType: type,
+    secondsTimestamp: Date.now()
+  };
+
+  alerts.push(alert);
+
+  updateDrivingRecordBanner();
+
+  //todo: UI stuff
+
+  if (points > 0) {
+    points = points - 10;
+  }
+  drivingScoreElem.innerHTML = points;
+}
+
+// function calcGForce(speed, wheelangle) {
+//
+//     var mps = speed / 3.6;
+//     console.log('mps: ', mps);
+//     var acceleration = Math.pow(mps, 2) / wheelangle;
+//     console.log('acceleration: ', acceleration);
+//     var gs = Math.abs(acceleration / 9.8);
+//
+//     gsElem.innerHTML = gs;
+//
+// }
